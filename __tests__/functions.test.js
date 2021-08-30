c6f02ea993a3b929f10bd30c70120e30c170982d
Nick Smedira <nsmedira@me.com>
2021-08-30 11:32:43 -0400

    implement test for sumArrayByKey

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -1,3 +1,21 @@
+import { expect } from "@jest/globals";
 import { sumArrayByKey } from "../functions";
 
-test("given an array of uniformly shaped objects, sum the values contained at a specified key", () => {});
+test("given an array of uniformly shaped objects, sum the values contained at a specified key", () => {
+  // arrange
+  const input = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 },
+    { value: 4 },
+    { value: 5 },
+  ];
+
+  const expectedValue = 15;
+
+  // act
+  const actualValue = sumArrayByKey(input, "value");
+
+  // assertions
+  expect(actualValue).toBe(expectedValue);
+});
